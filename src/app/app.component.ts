643f654a370973201a7c6706dc3c882f4e1571dd
Betsabe Junior Hoyos Barrios <betsabehoyos@gmail.com>
2025-07-08 17:49:08 -0500

    firt commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {  RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { CommonModule } from '@angular/common';
+import { UserTableComponent } from "./components/user-table/user-table.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CommonModule, UserTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'PruebaItHealth';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CreateUserComponent } from './pages/create-user/create-user.component';
+import { UserTableComponent } from './components/user-table/user-table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'crear-usuario', component: CreateUserComponent},
+    {path: 'inicio', component: UserTableComponent},
+    {path:'' , redirectTo: 'inicio', pathMatch: 'full'},
+];
